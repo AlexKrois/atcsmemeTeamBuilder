bea5037a6f25f9c23fba100449670c33f8974a97
Alexander Krois <krois@student.tugraz.at>
2024-04-25 22:49:36 +0200

    sinusbot script

---
+++ moveman.js
@@ -0,0 +1,46 @@
+//SinusBot Plugin, moved Leute automatisch in den richtigen Teamchannel.
+
+registerPlugin({
+    name: 'Faceit Mover',
+    version: '1.0.0',
+    backends: ['ts3', 'discord'],
+    description: 'Move Users according to created Teams',
+    author: 'alexvcs',
+
+}, () => {
+    const engine = require('engine')
+    const backend = require('backend')
+    const event = require('event')
+    var channels = backend.getChannels();
+
+    channels.forEach(function(channel) {
+      engine.log(channel.name());
+    });
+
+    event.on('load', () => {
+        const PLAYBACK = 1 << 12;
+        const command = require('command');
+        if (!command) {
+            engine.log('command.js library not found! Please download command.js to your scripts folder and restart the SinusBot, otherwise this script will not work.');
+            engine.log('command.js can be found here: https://github.com/Multivit4min/Sinusbot-Command/blob/master/command.js');
+            return;
+        }
+        const { createCommand } = command;
+
+        createCommand('moveallusers')
+            .addArgument(args => args.string.setName('username'))
+            .help('Register a new user')
+            .manual('Registers a new user bound to the Account you are using. This account has no privileges by default but can be edited by the bot administrators.')
+            .exec((client, args, reply, ev) => {
+                let users = engine.getUsers();
+                for(let i = 0; i < users.length; i++){
+                      var username = users[i].name();
+                      var tsUid = users[i].tsUid();
+                      var uid = users[i].uid();
+                      //HIER OUTPUT VOM PYTHON PROGRAMM EINFÜGEN!!!!!
+
+
+                }
+            });
+    })
+})
